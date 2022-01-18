d618f3d78de83e37b0408e16f8f7ca195dd72583
Jeffyter <jeffytermatos@gmail.com>
2022-01-18 14:52:13 -0300

    Criando biblioteca para analise de markdown

--- index.js
+++ index.js
@@ -1,11 +1,53 @@
 const chalk = require ("chalk");
 const fs = require ("fs");
 
-function pegaArquivo (caminhoDoArquivo) {
+function extraiLinks (texto) {
+  const regex = /\[([^\]]*)\]\((https?:\/\/[^$#\s].[^\s]*)\)/gm;
+  const arrayResultados = [];
+  let temp;
+  while ((temp = regex.exec (texto)) !== null) {
+    arrayResultados.push ({[temp[1]]: temp[2]});
+  }
+  return arrayResultados;
+}
+
+function trataErro (erro) {
+  throw new Error (chalk.red (erro
+    .code, "Não há arquivo no caminho."));
+}
+
+//tratando de forma assincrona promises com async e await
+
+async function pegaArquivo (caminhoDoArquivo) {
+  const encoding = "UTF-8";
+  try {
+    const texto = await fs.promises
+      .readFile (caminhoDoArquivo, encoding);
+    console.log (extraiLinks (texto));
+  } catch (erro) {
+    trataErro (erro);
+  }
+  
+}
+
+//tratando de forma assincrona promises com .then e .catch
+/* function pegaArquivo (caminhoDoArquivo) {
+  const encoding = "UTF-8";
+  fs.promises
+    .readFile (caminhoDoArquivo, encoding)
+    .then ((texto) => console.log (chalk.green (texto)))
+    .catch ((erro) => trataErro (erro));
+} */
+
+//tratando do markdown de forma sincrona em texto pequeno
+/* function pegaArquivo (caminhoDoArquivo) {
   const encoding = "UTF-8"
-  fs.readFile (caminhoDoArquivo, encoding, (_, texto) => {
+  fs.readFile (caminhoDoArquivo, encoding, (erro, texto) => {
+    if (erro) {
+      trataErro (erro);
+    }
     console.log (chalk.green (texto));
   })
-}
+} */
 
 pegaArquivo ("./arquivos/texto1.md");
