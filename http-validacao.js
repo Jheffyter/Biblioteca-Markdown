1828dc436de6fa4b708ca8f29a45606cdd48d36e
Jeffyter <jeffytermatos@gmail.com>
2022-01-21 12:03:14 -0300

    Criando biblioteca para analise de markdown

--- http-validacao.js
+++ http-validacao.js
@@ -1,9 +1,26 @@
-function geraArrayDeURLs (arrayLinks) {
-  return arrayLinks.map (objetoLink => Object.values(objetoLink).join ());
+const fetch = (...args) => import ("node-fetch")
+  .then (({default: fetch}) => fetch (...args));
+
+async function checaStatus (arrayURLs) {
+  const arrayStatus = await Promise
+    .all (arrayURLs
+      .map (async url => {
+        const res = await fetch (url);
+        return res.status;
+      }));
+      return arrayStatus;
 }
 
-function validaURLs (arrayLinks) {
-  return geraArrayDeURLs(arrayLinks);
+function geraArrayDeURLs (arrayLinks) {
+  return arrayLinks
+    .map (objetoLink => Object
+      .values (objetoLink).join ());
+}
+
+async function validaURLs (arrayLinks) {
+  const links = geraArrayDeURLs (arrayLinks);
+  const statusLinks = await checaStatus (links);
+  return statusLinks;
 }
 
 module.exports = validaURLs;
